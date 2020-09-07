163b3335adeb97b0a86da4975ed4087786880f0b
Heri Risnanto <sparta.nibel@gmail.com>
2020-09-07 11:56:40 +0700

    Nav Bar Mobile v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ const main = async () => {
   console.log('main.js');
   const hamburgerMenuElem = document.querySelector('#btn-hamburger');
   const menuDrawerElem = document.querySelector('#nav-drawer');
+  const headerElem = document.querySelector('header');
   const mainElem = document.querySelector('main');
 
   hamburgerMenuElem.addEventListener('click', event => {
@@ -11,11 +12,17 @@ const main = async () => {
     console.log('hamburger');
   });
 
+  headerElem.addEventListener('click', event => {
+    menuDrawerElem.classList.remove('is-open');
+    event.stopPropagation();
+    console.log('hamburger close');
+  });
+
   mainElem.addEventListener('click', event => {
     menuDrawerElem.classList.remove('is-open');
     event.stopPropagation();
     console.log('hamburger close');
-  })
+  });
 }
 
 export default main;
